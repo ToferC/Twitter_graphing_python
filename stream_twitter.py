1b94b5e762fedddfd20b1b698c2de0f0dfab0f47
Cgeist7 <cgeist7@gmail.com>
2015-10-18 21:32:51 -0400

    Update to machine learning notebooks

--- stream_twitter.py
+++ stream_twitter.py
@@ -1,11 +1,17 @@
 # Code modified from:
 # http://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
 
+import tweepy
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+import json
+import csv
 
 import os
 import argparse
+
+
+RESULTS_DIR = "streaming_results"
 
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
@@ -29,10 +35,17 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 class MyListener(StreamListener):
 
     def on_data(self, data):
+
         try:
-            with open('python.json', 'a') as f:
+            with open(os.path.join(RESULTS_DIR, '{}_stream.json'.format(query)), 'a') as f:
                 f.write(data)
+
+                result = json.loads(data)
+
+                print(result['text'].encode('ascii', 'ignore'), result['user']['screen_name'])
+                print("*****\n")
                 return True
+
         except BaseException as e:
             print("Error on_data: %s" % str(e))
         return True
